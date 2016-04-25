8bc09a0875a643fdbe3b39821a0d16b6b80b235b
moebaca <moebaca@hotmail.com>
2016-04-25 01:19:26 +0000

    Add guestbook modal

--- telkins-front-end/app/router.js
+++ telkins-front-end/app/router.js
@@ -6,7 +6,7 @@ const Router = Ember.Router.extend({
 });
 
 Router.map(function() {
-  this.route('guestbooks');
+  this.route('guestbooks', {path: '/guestbooks'});
 });
 
 export default Router;
--- telkins-front-end/app/routes/guestbooks.js
+++ telkins-front-end/app/routes/guestbooks.js
@@ -3,5 +3,23 @@ import Ember from 'ember';
 export default Ember.Route.extend({
     model() {
     return this.store.findAll('guestbook');
+  },
+  
+  actions: {
+    openCheckoutModal(guestbook) {
+      return this.render('components/new-guestbook-modal', {
+        outlet: 'modal',
+        into: 'application',
+        model: guestbook,
+        controller: 'application'
+      });
+    },
+
+    closeCheckoutModal() {
+      return this.disconnectOutlet({
+        outlet: 'modal',
+        parentView: 'application'
+      });
+    }
   }
 });
